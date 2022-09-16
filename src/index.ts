ce69490c4467f5d966b7862b7e41cd70bdf570dc
sya-ri <git@s7a.dev>
2022-09-16 10:28:10 +0900

    feat: Implement

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+	WEBDAV_URL: string;
+	WEBDAV_USER: string;
+	WEBDAV_PASS: string;
 }
 
 export default {
 	async fetch(
 		request: Request,
 		env: Env,
-		ctx: ExecutionContext
+		// ctx: ExecutionContext
 	): Promise<Response> {
-		return new Response("Hello World!");
+		const { pathname } = new URL(request.url);
+		const response = await fetch(
+			`${env.WEBDAV_URL}/${pathname}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: "Basic " + btoa(`${env.WEBDAV_USER}:${env.WEBDAV_PASS}`),
+				}
+			}
+		);
+		if (response.ok) {
+			return new Response(response.body);
+		}
+		return new Response("File not found", { status: 404 });
 	},
 };
